bd302d9f6af9f4df6164597ef4fa5464f55a16fb
Simon Murtha-Smith <simon@murtha-smith.com>
2014-01-25 20:54:09 -0800

    some config test cleanup

--- test/config.test.js
+++ test/config.test.js
@@ -4,11 +4,26 @@ var config = require('config');
 
 describe('config', function() {
   describe('env', function() {
-    it('should load env vars', function() {
+    it('should load env vars', function(done) {
       config.load(null, null, function() {
-        console.error('config', config);
         assert(Object.keys(config).length > 2);
+        done();
       });
     });
   });
+
+  /*
+  describe('etcd', function() {
+    it('should callback with an error if etcd is not available', function(done) {
+      try {
+        config.load('etcd', '127.0.0.1', function(err) {
+          assert(err);
+          done();
+        });
+      } catch(err) {
+        assert(err);
+        done();
+      }
+    });
+  });*/
 });
